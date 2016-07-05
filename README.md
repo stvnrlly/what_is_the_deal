9b06d12f8a217ce358ee785ece0cf915139d7e97
stvnrlly <stvnrlly@stvnrlly.com>
2016-07-04 20:57:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# whats-the-deal
+
+Using random words from the [Wordnik](https://www.wordnik.com/) API and related
+concepts from [Concept Net](http://conceptnet5.media.mit.edu/) to ask the
+eternal question.
+
+## Installation
+
+You'll need (1) a Wordnik API key and (2) Twitter API keys, as specified in the
+[Twit documentation](https://github.com/ttezel/twit) and created in [Twitter's
+app management console](https://apps.twitter.com/). Add those to a `.env` file like so:
+
+```
+WORDNIK_API_KEY=your_api_key
+TWITTER_CONSUMER_KEY=your_consumer_key
+TWITTER_CONSUMER_SECRET=your_consumer_secret
+TWITTER_ACCESS_TOKEN=your_access_token
+TWITTER_ACCESS_TOKEN_SECRET=your_access_token_secret
+```
+
+Then:
+
+```
+npm install
+npm start
+```
+
+## License
+
+This work is dedicated to the public domain. Copyright is waived under a [CC0-1.0 license](LICENSE.md).
---
+++ app.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var ConceptNet = require('concept-net');
+var natural = require('natural');
+var Wordnik = require('wordnik');
+var Twit = require('twit');
+require('dotenv').config();
+
+// Connect to concept net
+var conceptNet = new ConceptNet();
+
+// Initialize noun inflector to change singular ↔ plural
+var nounInflector = new natural.NounInflector();
+
+// Set up Wordnik API
+var wn = new Wordnik({
+  api_key: process.env.WORDNIK_API_KEY
+});
+
+// Set up Twitter API
+var T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var deals = ['What\'s', 'What is'];
+
+var tweet = function(text) {
+  T.post('statuses/update', {
+    status: text
+  }, function(err, data, response) {
+    if (err) {
+      console.log(err);
+    }
+    // console.log(data);
+  });
+};
+
+function getConcepts(thing) {
+  conceptNet.search({
+    rel: ['/r/IsA', '/r/DefinedAs','/r/RelatedTo'],
+    end: '/c/en/'+thing
+  }, function onDone( err, result ) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result.edges.length > 1) {
+        var deal = deals[Math.floor(Math.random() * (deals.length))];
+        for (var i = 0; i < 20; i++) {
+          var first = result.edges[Math.floor(Math.random() * (result.edges.length))].surfaceStart;
+          var second = result.edges[Math.floor(Math.random() * (result.edges.length))].surfaceStart;
+          if (first && second && (first !== second)) {
+            if ((first.split(' ').length === 1) && (first.slice(-1) !== 's')) {
+              first = nounInflector.pluralize(first);
+            }
+            if ((second.split(' ').length === 1) && (second.slice(-1) !== 's')) {
+              second = nounInflector.pluralize(second);
+            }
+            var query = deal+' the deal with '+nounInflector.pluralize(thing)+
+              '? Are they '+first+' or are they '+second+'?';
+            tweet(query);
+            break;
+          }
+        }
+      }
+    }
+  });
+}
+
+function getWord(callback) {
+  wn.randomWord({
+    minCorpusCount: 10000,
+    includePartOfSpeech: ['noun'],
+    excludePartOfSpeech: ['noun-plural','proper-noun']
+  }, function (err, word) {
+    if (err) {
+      console.log(err);
+    }
+    getConcepts(nounInflector.singularize(word.word));
+  });
+}
+
+getWord();
